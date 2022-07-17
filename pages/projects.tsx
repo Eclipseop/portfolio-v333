6cb0d40c19f493321608e7fd439c4809a25a24c4
eclipseop <mh@otterdev.com>
2022-07-17 12:47:55 -0500

    update github card to be closer to what notion actually shows

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 import { useEffect, useState } from "react";
 import Layout from "../components/Layout"
 import { GHInformation } from "../types/GHInformation";
-import { FaStar } from "react-icons/fa";
+import { FaStar, FaGithub } from "react-icons/fa";
 
 const Card = ({ repo }: { repo: GHInformation }) => {
   return (
@@ -16,8 +16,12 @@ const Card = ({ repo }: { repo: GHInformation }) => {
 
 const GHCard = ({ repo }: { repo: GHInformation }) => {
   return (
-    <a target="_blank" className="py-1.5 rounde px-2 border flex gap-3 items-center hover:bg-gray-100 transition duration-150" href={repo.html_url} rel="noreferrer">
+    <a target="_blank" className="py-1.5 rounde px-2 border flex items-center hover:bg-gray-100 transition duration-150" href={repo.html_url} rel="noreferrer">
       <img src={repo.owner.avatar_url} alt="owner avatar" className="rounded-full h-8 w-8" />
+      <div className="relative -left-3 -bottom-2 bg-white rounded-full p-[1px]">
+        <FaGithub />
+      
+      </div>
       <div className="leading-none">
         <h2 className="text-sm font-semibold">{repo.name}</h2>
         <span className="text-xs text-gray-400 flex items-center gap-1">{repo.owner.login} • <FaStar /> {repo.stargazers_count} • Updated {repo.pushed_at.split('T')[0]}</span>
